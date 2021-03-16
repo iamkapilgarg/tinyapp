127c3985170b80b3ff37270917f8b39209fc4b52
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-15 20:09:20 -0700

    added new url view

--- express_server.js
+++ express_server.js
@@ -18,6 +18,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars); //EJS will automatically search this file in views folder.
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars); //EJS will automatically search this file in views folder.
